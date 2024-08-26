7dd3a277fa948911a4a50a60f7cbfe054dce96c8
yksl <yukselkurtuldu9@gmail.com>
2024-08-26 22:37:55 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+"use strict"
+/* -------------------------------------------------------
+    | Mind Canvas |
+------------------------------------------------------- */
+
+const express = require('express')
+const app = express()
+
+/* ------------------------------------------------------- */
+// Required Modules:
+
+// envVariables to process.env:
+require('dotenv').config()
+const HOST = process.env?.HOST || '127.0.0.1'
+const PORT = process.env?.PORT || 8000
+
+// asyncErrors to errorHandler:
+require('express-async-errors')
+
+/* ------------------------------------------------------- */
+// Configrations:
+
+// Connect to DB:
+const { dbConnection } = require('./src/configs/dbConnection')
+dbConnection()
+
+/* ------------------------------------------------------- */
+// Middlewares:
+
+// Accept JSON:
+app.use(express.json())
+
+
+/* ------------------------------------------------------- */
+// Routes:
+
+
+
+
+
+/* ------------------------------------------------------- */
+
+// errorHandler:
+app.use(require('./src/middlewares/errorHandler'))
+
+// RUN SERVER:
+app.listen(PORT, HOST, () => console.log(`http://${HOST}:${PORT}`))
---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,17 @@
+"use strict"
+/* -------------------------------------------------------
+    | Mind Canvas |
+------------------------------------------------------- */
+
+// app.use(errorHandler):
+
+module.exports = (err, req, res, next) => {
+
+    return res.status(res?.errorStatusCode || 500).send({
+        error: true,
+        message: err.message,
+        cause: err.cause,
+        body: req.body,
+        stack: err.stack
+    });
+}
