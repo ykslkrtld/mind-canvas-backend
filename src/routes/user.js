54f66f6c0ccef5178f211a7706ab0e5390c81a43
yksl <yukselkurtuldu9@gmail.com>
2024-08-26 22:46:20 +0300

    user model start

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,3 +2,20 @@
 /* -------------------------------------------------------
     | Mind Canvas |
 ------------------------------------------------------- */
+
+const { mongoose } = require('../configs/dbConnection')
+
+/* ------------------------------------------------------- */
+
+const UserSchema = new mongoose.Schema(
+    {
+        
+
+}, 
+
+{ collection: "users", timestamps: true }
+
+);
+
+module.exports = mongoose.model("User", UserSchema)
+
