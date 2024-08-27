9be5ed667b4a222ece181669fa051794b17686eb
yksl <yukselkurtuldu9@gmail.com>
2024-08-27 12:03:48 +0300

    user model

---
+++ src/helpers/passwordEncrypt.js
@@ -0,0 +1,16 @@
+"use strict"
+/* -------------------------------------------------------
+    | Mind Canvas |
+------------------------------------------------------- */
+
+// passwordEncrypt(password:string):
+
+const { pbkdf2Sync } = require('node:crypto'),
+    keyCode = process.env.SECRET_KEY,
+    loopCount = 1000,
+    charCount = 32,
+    encType = 'sha512';
+
+module.exports = function (password) {
+    return pbkdf2Sync(password, keyCode, loopCount, charCount, encType).toString('hex')
+}
--- src/models/user.js
+++ src/models/user.js
@@ -2,3 +2,59 @@
 /* -------------------------------------------------------
     | Mind Canvas |
 ------------------------------------------------------- */
+
+const { mongoose } = require('../configs/dbConnection')
+
+/* ------------------------------------------------------- */
+
+const UserSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: true,
+            index: true
+        },
+        password: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        email: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: true,
+            index: true,
+        },
+        firstName: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        lastName: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        isActive: {
+            type: Boolean,
+            default: true,
+        },
+        isStaff: {
+            type: Boolean,
+            default: false,
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
+}, 
+
+{ collection: "users", timestamps: true }
+
+);
+
+module.exports = mongoose.model("User", UserSchema)
+
