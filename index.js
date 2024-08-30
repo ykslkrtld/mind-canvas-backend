b99feb2190516d13a1a9f86bcbf3468fff73bb0c
yksl <yukselkurtuldu9@gmail.com>
2024-08-30 17:31:18 +0300

    up

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const cors = require('cors');
 app.use(cors({
     origin: [
         'http://localhost:3000', 
-        'https://mind-canvas-ykslkrtld.vercel.app'
+        'https://blog-app22.vercel.app'
     ]
 }));
 
