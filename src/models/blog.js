afadc591b77c5407257d38fffbe2acff98212965
yksl <yukselkurtuldu9@gmail.com>
2024-08-31 13:05:35 +0300

    up

--- src/models/blog.js
+++ src/models/blog.js
@@ -46,10 +46,10 @@ const BlogSchema = new mongoose.Schema(
         type: Number,
         default: 0
     },
-    // comments: [{
-    //     type: mongoose.Schema.Types.ObjectId,
-    //     ref: 'Comment'
-    // }],
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    }],
 
     },
     {collection: "blogs", timestamps: true}
