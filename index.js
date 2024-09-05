b03ac047f86ef16e0a152522c5a8164432ad8972
yksl <yukselkurtuldu9@gmail.com>
2024-09-05 21:38:20 +0300

    up

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 
 const express = require('express')
 const app = express()
+const path = require('path')
 
 /* ------------------------------------------------------- */
 
@@ -68,6 +69,11 @@ app.all('/', (req, res) => {
 app.use(require('./src/routes'))
 
 /* ------------------------------------------------------- */
+
+app.use(
+    "/swagger",
+    express.static(path.join(__dirname, "node_modules", "swagger-ui-dist"))
+  );
 
 // errorHandler:
 app.use(require('./src/middlewares/errorHandler'))
